5f197cbbdaedc6d98c6b8fe13c633f6caf97532c
jooseryan <jooseryan01@gmail.com>
2025-07-24 16:27:00 -0300

    Configura rotas para /import e /export com seus respectivos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './components/home/home';
 import { RegisterComponent } from './components/register/register';
 import { SearchComponent } from './components/search/search';
 import { UpdateComponent } from './components/update/update';
+import { ImportComponent } from './components/import/import';
+import { ExportComponent } from './components/export/export';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -14,7 +16,9 @@ export const routes: Routes = [
     children: [
       { path: 'register', component: RegisterComponent },
       { path: 'search', component: SearchComponent },
-      { path: 'update/:id', component: UpdateComponent }
+      { path: 'update/:id', component: UpdateComponent },
+      { path: 'import', component: ImportComponent },
+      { path: 'export', component: ExportComponent}
     ]
   },
   { path: '**', redirectTo: 'login' }
